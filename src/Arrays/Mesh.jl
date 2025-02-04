6b78eea4b39745ad1d68d174643f1feda9901e12
Carluer Taran <e21900772@ed105.univ-brest.fr>
2025-02-04 15:11:48 +0100

    work on mesh

--- src/Arrays/Mesh.jl
+++ src/Arrays/Mesh.jl
@@ -31,32 +31,51 @@ struct Mesh
 	
 	mskx
 	msky
+
+	#Orders
+	
 	
 	function Mesh(nx, ny, nh, msk, Lx = 1, Ly = 1)
 		#Locations
-		xc = zeros(nx,ny)
-		yc = zeros(nx,ny)
-		for i in 1:nx, j in 1:ny
-			xc[i,j] = (i-0.5-nh) * Lx/(nx-2*nh)
-			yc[i,j] = (j-0.5-nh) * Ly/(ny-2*nh)
-		end
+		xc, yc = compute_locations(nx,ny,nh,Lx,Ly)
 		
 		#Masks
-		mskx = zeros(nx, ny)
-		msky = zeros(nx, ny)
-		mskv = zeros(nx, ny)
-
-		for i in 2:nx-1, j in 2:ny-1
-			mskx[i, j] = msk[i+1, j] * msk[i, j]
-			msky[i, j] = msk[i, j+1] * msk[i, j]
-			mskv[i, j] = msk[i, j] * msk[i+1, j] * msk[i, j+1] * msk[i+1, j+1]
-		end
-
+		mskx, msky, mskv = compute_msks(msk)
+		
+		#Creating the mesh
 		return new(nx, ny, nh,
 			   xc, yc, 1, 1, 1, 1, 1, 1,
 			   1, 1, 1, 
 			   msk, mskv, mskx, msky)
 	end
+end
+
+function compute_locations(nx, ny, nh, Lx, Ly)
+	xc = zeros(nx,ny)
+	yc = zeros(nx,ny)
+	for i in 1:nx, j in 1:ny
+		xc[i,j] = (i-0.5-nh) * Lx/(nx-2*nh)
+		yc[i,j] = (j-0.5-nh) * Ly/(ny-2*nh)
+	end
+	
+	return xc, yc
+end
+
+
+function compute_msks(msk)
+	nx, ny = shape(msk)
+
+	mskx = zeros(nx, ny)
+	msky = zeros(nx, ny)
+	mskv = zeros(nx, ny)
+
+	for i in 2:nx-1, j in 2:ny-1
+		mskx[i, j] = msk[i+1, j] * msk[i, j]
+		msky[i, j] = msk[i, j+1] * msk[i, j]
+		mskv[i, j] = msk[i, j] * msk[i+1, j] * msk[i, j+1] * msk[i+1, j+1]
+	end
+	
+	return mskx, msky, mskv
 end
 
 dx = ArrayVariable("dx")
