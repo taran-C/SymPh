2faa2bc0d6d8e51db51df16c5950352ce683bda6
Carluer Taran <e21900772@ed175.univ-brest.fr>
2025-06-10 15:26:42 +0200

    idk

---
+++ examples/conv_boussinesq.jl
@@ -0,0 +1,180 @@
+using GLMakie
+using LinearRegression
+using GeometryBasics
+using ColorSchemes
+using SymPh
+using SymPh.Maths
+import SymPh.Arrays
+using LoopManagers: PlainCPU, VectorizedCPU, MultiThread
+using FFTW
+using Statistics
+
+#Defining our equation
+@Let omega = FormVariable{2, Dual}() #Vorticity
+@Let b = FormVariable{0, Dual}() #Buoyancy
+@Let dphi = FormVariable{1, Dual}() #dφ (d of geopotential)
+@Let stratif = FormVariable{0, Dual}()
+
+@Let psi = InverseLaplacian(omega) #∇²ω = Ψ
+@Let u = Codifferential(psi) #u = δΨ
+@Let U = Sharp(u)
+@Let dhb = ExteriorDerivative(b + stratif)
+#Time derivative
+@Let dtomega = ExteriorDerivative(Wedge(b + stratif,dphi)) #dtω = L(U,ω) - d(b∧dφ)
+@Let dtb = -InteriorProduct(U, dhb) #dtb = L(U,b) + forcing term #TODO there is a boundary problem here
+
+#Defining the parameters needed to explicit
+explparams = ExplicitParam(; interp = Arrays.avg2pt, fvtofd = Arrays.fvtofd2)
+
+#Generating the RHS
+#rhs! = to_kernel(dtomega, dtb; save = ["U_X", "U_Y", "u_i", "u_j", "ι_U_omega_i", "ι_U_omega_j", "ι_U_db", "db_i", "db_j"], explparams = explparams, verbose = false, bcs=[U, psi, dtomega, dtb])
+
+N = 1 #Brunt Vaiasala Frequency, we set N,g, dphi etc to 1, easier
+
+inner(mesh) = (mesh.nh+1:mesh.ni-mesh.nh, mesh.nh+1:mesh.nj-mesh.nh) 
+#ANALYTICAL SOLUTION --------------------------------------------
+function ics(hhat, mesh)
+	hhat[inner(mesh)...] .= FFTW.r2r(hhat[inner(mesh)...], FFTW.REDFT10)
+end
+
+#Dispersion relations
+function get_disp(mesh; model = "internal")
+	omx, omy = pi .* (collect(inner(mesh)[1]) .- mesh.nh .-1), pi .* (collect(inner(mesh)[2]) .- mesh.nh .-1) #-1 because JULIA INDEXES AT 1 !!!!!
+        ox = omx * ones(mesh.nj-2*mesh.nh)'
+        oy = ones(mesh.ni-2*mesh.nh) * omy'
+	k2 = (ox .^2 .+ oy .^2)
+
+        if model == "internal"
+                internal = N * abs.(ox) ./ sqrt.(k2)
+                internal[k2 .== 0] .= 0
+                return internal
+        else
+
+        end
+end
+
+#Spectral solution at time t, is the ics transported by cos(ωt)
+function sol_at_t(hhat, omega, t, mesh)
+	hh = hhat[inner(mesh)...] .* cos.(omega .* t)
+        
+	h = FFTW.r2r(hh, FFTW.REDFT01) ./(4*(mesh.ni-2*mesh.nh)*(mesh.nj-2*mesh.nh)) # no factor since FFTW normalizes dct
+
+        return h
+end
+#----------------------------------------------------------------
+
+function check_symmetry(q, mesh; anti = false, way = "xy")
+	ass = zero(q)
+	if anti
+		c = -1
+	else
+		c = 1
+	end
+	for i in 1:mesh.ni, j in 1:mesh.nj
+		if way == "xy"
+			ass[i,j] = q[i,j] - c* q[mesh.ni-i+1, mesh.nj-j+1]
+		elseif way == "x"
+			ass[i,j] = q[i,j] - c* q[mesh.ni-i+1, j]
+		elseif way == "y"
+			ass[i,j] = q[i,j] - c* q[i, mesh.nj-j+1]
+		end
+	end
+	return ass
+end
+
+
+#Testing the function
+pows = 5:5
+h = 1 ./(2 .^collect(pows))
+errs = zero(h)
+
+for (i,pow) in enumerate(pows)
+	#Regenerate the kernel, lame, needs a way to just reset Poisson solver
+	rhs! = to_kernel(dtomega, dtb; save = ["U_X", "U_Y", "u_i", "u_j", "dhb_i", "dhb_j", "ι_U_omega_i", "ι_U_omega_j", "ι_U_db", "db_i", "db_j"], explparams = explparams, verbose = false, bcs=[U, psi, dtomega, dtb, dhb])
+	
+	#Defining the Mesh
+	nh = 4
+	ni = 2^pow + 2*nh
+	nj = 2^pow + 2*nh
+
+	#LoopManager
+	simd = VectorizedCPU(16)
+
+	mesh = Arrays.CartesianMesh(ni, nj, nh, simd; xperio = false, yperio = false)
+
+	#Initial Conditions
+	state = State(mesh)
+	
+	gaussian(x,y,x0,y0,sigma) = exp(-((x-x0)^2 + (y-y0)^2)/(2*sigma^2))
+	for i in 1:ni, j in 1:nj
+		x = mesh.xc[i,j]
+		y = mesh.yc[i,j]
+		
+		x0, y0, σ, amp = 0.5, 0.5, 0.047, 1e-8
+
+		state.stratif[i,j] = y * N #* mesh.msk0d[i,j]
+		state.b[i,j] = amp * gaussian(x, y, x0,y0,σ)# * mesh.msk0d[i,j]
+		state.hhat[i,j] = amp * gaussian(x, y, x0,y0,σ)# * mesh.msk0d[i,j]
+	end
+	ics(state.hhat, mesh)
+
+	state.dphi_j .= N .* mesh.dy #Technically dz but... eh. Defining personalized dimension names would be cool though
+
+	#Creating the Model
+	model = Model(rhs!, mesh, state, ["omega", "b"]; cfl = 0.001, dtmax = 1/(2^pow), integratorstep! = euler_forwardstep!)
+
+	#Running the simulation
+	T = 15
+	nite = 2^(pow) * T
+	#display(sum(state.omega))
+	run!(model; save_every = 1, tend = 1500, maxite = nite, writevars = (:u_i, :u_j, :omega, :b, :psi, :db_i, :db_j, :dtb))
+	
+	#getting spectral solution
+	state.h_th[inner(mesh)...] .= sol_at_t(state.hhat, get_disp(mesh), model.t, mesh)
+
+	#Comparison
+	residue = state.b[inner(mesh)...] - state.h_th[inner(mesh)...]
+	Linf(A) = maximum(abs.(A))
+	rms(A) = sqrt(mean(A .^ 2)) #Root Mean Square
+	errs[i] = Linf(residue)
+
+	plot = true
+	#Plotting
+	if plot
+		fig = Figure()
+		ax1 = Axis(fig[1,1], aspect=1)
+		ax2 = Axis(fig[1,3], aspect=1)
+		ax3 = Axis(fig[2,1], aspect=1)
+		ax4 = Axis(fig[2,3], aspect=1)
+
+		hms = true
+		maxint = max(maximum(abs.(state.b)), maximum(abs.(state.h_th)))
+		crange = (-maxint, maxint)
+
+		
+		if hms
+			hm1 = heatmap!(ax1, state.b; colormap=:balance, colorrange = crange)
+			Colorbar(fig[1,2], hm1)
+			
+			hm2 = heatmap!(ax2, state.h_th; colormap=:balance, colorrange = crange)
+			Colorbar(fig[1,4], hm2)
+
+			hm3 = heatmap!(ax3, residue; colormap=:balance, colorrange = (-maximum(abs.(residue)), maximum(abs.(residue))))
+			Colorbar(fig[2,2], hm3)
+			
+			hm4 = heatmap!(ax4, check_symmetry(state.b, mesh; anti = false, way="y"); colormap=:balance)
+			Colorbar(fig[2,4], hm4)
+
+		else
+			plot!(ax1, state.dhb_j[ni÷2,:])
+			plot!(ax2, state.h_th[ni÷2,:])
+			plot!(ax3, state.dhb_i[:, nj÷2])
+			plot!(ax4, state.h_th[:, nj÷2])
+		end
+
+		display(fig)
+	end
+end
+order = LinearRegression.slope(linregress(log.(h), log.(errs)))[1]
+println("O = $order")
+display(errs)
---
+++ src/Poisson2D_old.jl
@@ -0,0 +1,128 @@
+using LinearAlgebra, SparseArrays
+
+export Poisson2D, solve_poisson
+
+mutable struct Poisson2D
+	bc
+	form_type
+	poisson_solver
+end
+Poisson2D(bc, form_type) = Poisson2D(bc, form_type, nothing)
+function solve_poisson(poisson::Poisson2D, mesh, out, b)
+	if poisson.poisson_solver == nothing
+		poisson.poisson_solver = get_poisson_solver(mesh, poisson.bc, poisson.form_type)
+	end
+	poisson.poisson_solver(out, b)
+end
+
+
+function laplacian(mesh, msk, bc, location)
+	@assert bc in ["dirichlet", "neumann"]
+	@assert location in ["center", "vertex"]
+	
+	G = zeros(Int32, mesh.ni, mesh.nj)
+	G[msk .> 0] .= collect(1:length(G[msk .> 0]))
+	G[msk .== 0] .= -1
+	
+	
+	N = sum(G .> -1)
+
+	vals = zeros(0)
+	rows = zeros(0)
+	cols = zeros(0)
+	counter = 1
+
+	function add_entry(val, r, c, counter)
+		push!(vals, val)
+		push!(rows, r)
+		push!(cols, c)
+		return counter +1
+	end
+
+	for j in 1:mesh.nj
+		for i in 1:mesh.ni
+			I = G[i,j]
+		
+			#TODO explain why (someting to do with hodge in codif in laplacian i think)
+			dx2 = mesh.dy[i,j] / mesh.dx[i,j]
+			dy2 = mesh.dx[i,j] / mesh.dy[i,j]
+			
+			if I>-1
+				s = 0
+				#TODO Different halos for different forms
+				west = i>mesh.nh+1 ? G[i-1, j] : (mesh.iperio ? G[mesh.ni-mesh.nh, j] : -1)
+				east = i<mesh.ni-mesh.nh-1 ? G[i+1, j] : (mesh.iperio ? G[mesh.nh+1, j] : -1)
+				south = j>mesh.nh+1 ? G[i, j-1] : (mesh.jperio ? G[i, mesh.nj-mesh.nh] : -1)
+				north = j<mesh.nj-mesh.nh-1 ? G[i, j+1] : (mesh.jperio ? G[i, mesh.nh+1] : -1)
+				
+				if west > -1
+					counter = add_entry(dx2, I, west, counter)
+					s += dx2
+				end
+
+				if east > -1
+					counter = add_entry(dx2, I, east, counter)
+					s += dx2
+				end
+
+				if north > -1
+					counter = add_entry(dy2, I, north, counter)
+					s += dy2
+				end
+
+				if south > -1
+					counter = add_entry(dy2, I, south, counter)
+					s += dy2
+				end
+
+				if bc == "dirichlet" #Closed border
+					if location == "vertex"
+						counter = add_entry(-2*(dx2+dy2), I, I, counter)
+					elseif location == "center"
+						#TODO check (mostly for different grids)
+						counter = add_entry(-4*(dx2+dy2) + s, I, I, counter)
+					end
+				elseif bc == "neumann" #Open border
+					counter = add_entry(-s, I, I, counter)
+				end
+			end
+		end
+	end
+
+	A = factorize(sparse(rows, cols, vals, N, N))
+	#display(sparse(rows, cols, vals, N, N))
+	return A
+end
+
+"""
+	solve_poisson(out, b)
+
+Solves the poisson problem Ax = b and sets out to be x
+"""
+function get_poisson_solver(mesh, bc, form_type)
+	@assert bc in ["dirichlet", "neumann"]
+	@assert form_type in ["0p", "0d", "2p", "2d"]
+
+	if form_type == "0p" #TODO check
+		location = "center"
+		msk = mesh.msk0p
+	elseif form_type == "0d"
+		location = "vertex"
+		msk = mesh.msk0d
+	elseif form_type == "2p"
+		location = "vertex"
+		msk = mesh.msk2p
+	elseif form_type == "2d"
+		location = "center"
+		msk = mesh.msk2d
+	end
+
+	A = laplacian(mesh, msk, bc, location)
+
+	#Create a function that solves Ax = b
+	function func!(out, b)
+		out[msk .> 0] = A\b[msk .> 0]
+	end
+
+	return func!
+end
